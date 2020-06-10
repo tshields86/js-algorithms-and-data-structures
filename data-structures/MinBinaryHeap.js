7c9a28d8fdb4cac03ca18a302c4226e51f69118e
Travis Shields <travis.shields@gmail.com>
2020-06-10 19:33:25 -0400

    Add MinBinaryHeap structure

---
+++ data-structures/MinBinaryHeap.js
@@ -0,0 +1,76 @@
+class MinBinaryHeap {
+  constructor() {
+    this.items = [];
+    this.size = 0;
+  }
+
+  insert(item) {
+    this.items[this.size] = item;
+    this.size++;
+    this.heapifyUp();
+  }
+
+  poll() {
+    if (this.isEmpty()) return;
+    this.swap(0, this.size - 1);
+    const item = this.items.pop();
+    this.size--;
+    this.heapifyDown();
+    return item;
+  }
+
+  heapifyUp() {
+    let idx = this.size - 1;
+    while (this.hasParent(idx) && this.parent(idx) > this.items[idx]) {
+      let parentIdx = this.parentIdx(idx);
+      this.swap(parentIdx, idx);
+      idx = parentIdx;
+    }
+  }
+
+  heapifyDown() {
+    let idx = 0;
+    while (this.hasLeft(idx)) {
+      let smallerChildIdx = this.leftIdx(idx);
+      if (this.hasRight(idx) && this.right(idx) < this.left(idx)) {
+        smallerChildIdx = this.rightIdx(idx);
+      }
+
+      if (this.items[idx] < this.items[smallerChildIdx]) break;
+
+      this.swap(idx, smallerChildIdx);
+      idx = smallerChildIdx;
+    }
+  }
+
+  peek() {
+    if (this.isEmpty()) return;
+    return this.items[0];
+  }
+
+  size() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  swap(idx1, idx2) {
+    [this.items[idx1], this.items[idx2]] = [this.items[idx2], this.items[idx1]];
+  }
+
+  leftIdx(parentIdx) { return 2 * parentIdx + 1; }
+  rightIdx(parentIdx) { return 2 * parentIdx + 2; }
+  parentIdx(childIdx) { return Math.floor((childIdx - 1) / 2); }
+
+  hasLeft(idx) { return this.leftIdx(idx) < this.size; }
+  hasRight(idx) { return this.rightIdx(idx) < this.size; }
+  hasParent(idx) { return this.parentIdx(idx) >= 0; }
+
+  left(idx) { return this.items[this.leftIdx(idx)]; }
+  right(idx) { return this.items[this.rightIdx(idx)]; }
+  parent(idx) { return this.items[this.parentIdx(idx)]; }
+};
+
+module.exports = MinBinaryHeap;
